75b1a0dbb75b9b8e014f38c753aa21422c6ba29a
hssainidev <37307761+hssainidev@users.noreply.github.com>
2022-08-09 10:32:27 -0600

    Added home page and reset to local db. Also removed apikeys from git repo.

--- frontend/myapp/src/environments/environment.ts
+++ frontend/myapp/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://hssaini.us-west-2.elasticbeanstalk.com',
+  apiBaseUrl: 'http://localhost:8080',
+  // apiBaseUrl: 'http://hssaini.us-west-2.elasticbeanstalk.com',
   externalApiBaseUrl: 'https://api.openweathermap.org/data/2.5',
   apiKey: 'f627533c361cd4ec09231f26d79e1fe6'
 };
